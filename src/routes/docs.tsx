dd3996332d63b5e5465e41a39e4b29da8af13434
jasokant-klick <josh@tryhellohire.com>
2025-05-23 03:28:13 -0400

    try swagger dist

--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,15 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
-import SwaggerUI from 'swagger-ui-react'
-import 'swagger-ui-react/swagger-ui.css'
+import { useEffect, useState } from 'react'
+import 'swagger-ui-dist/swagger-ui.css'
 
 export const Route = createFileRoute('/docs')({
   component: DocsPage,
 })
 
 function DocsPage() {
-  return (
-    <div className="h-screen w-full">
-      <SwaggerUI url="/api/docs/swaggerJson"/>
-    </div>
-  )
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+
+    import('swagger-ui-dist/swagger-ui-bundle.js').then(({ default: SwaggerUIBundle }) => {
+      SwaggerUIBundle({
+        dom_id: '#swagger-ui',
+        url: '/api/docs/swaggerJson',
+      })
+    })
+  }, [])
+
+  if (!mounted) return null
+
+  return <div id="swagger-ui" className="h-screen" />
 }
