58c893dca31a56b707357502fb37c98e4f6a36d7
jasokant-klick <josh@tryhellohire.com>
2025-05-26 22:51:48 -0400

    update food truck schema to account for missing statuses

--- src/schemas/foodTruck.ts
+++ src/schemas/foodTruck.ts
@@ -4,8 +4,8 @@ export const FoodTruckSchema = z.object({
   objectid: z.string(),
   applicant: z.string(),
   address: z.string(),
-  locationdescription: z.string(),
-  status: z.enum(['APPROVED', 'REQUESTED', 'EXPIRED']),
+  locationdescription: z.string().optional(),
+  status: z.enum(['APPROVED', 'REQUESTED', 'EXPIRED', 'SUSPEND', 'ISSUED']),
   fooditems: z.string().optional(),
   schedule: z.string().url().optional(),
   location: z.object({
