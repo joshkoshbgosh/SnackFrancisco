48be22100e597392cc1c28f3efe7b9fa9c14357d
jasokant-klick <josh@tryhellohire.com>
2025-05-28 03:36:53 -0400

    expose google maps api key publicly

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,16 @@
-import { createEnv } from '@t3-oss/env-core'
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
 
 export const env = createEnv({
-  server: {
-    GOOGLE_MAPS_API_KEY: z.string().min(1),
-  },
-  runtimeEnv: {
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-  },
+	server: {
+		GOOGLE_MAPS_API_KEY: z.string().min(1),
+	},
+	runtimeEnvStrict: {
+		GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+		VITE_GOOGLE_MAPS_API_KEY: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+	},
+	clientPrefix: "VITE_",
+	client: {
+		VITE_GOOGLE_MAPS_API_KEY: z.string().min(1),
+	},
 })
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,63 +1,65 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react"
 import {
-  Outlet,
-  createRootRouteWithContext,
-  HeadContent,
-  Scripts,
-} from '@tanstack/react-router'
+	Outlet,
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
+} from "@tanstack/react-router"
 import appCss from "@/styles/app.css?url"
-import type { QueryClient } from '@tanstack/react-query'
-import { env } from '@/env'
+import type { QueryClient } from "@tanstack/react-query"
+import { env } from "@/env"
 
-export const Route = createRootRouteWithContext<{queryClient: QueryClient}>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'SnackFrancisco',
-      },
-    ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
-    scripts: [
-      {
-        src: `https://maps.googleapis.com/maps/api/js?key=${env.GOOGLE_MAPS_API_KEY}&libraries=places`,
-        async: true,
-        defer: true,
-      },
-    ],
-  }),
-  component: RootComponent,
-})
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+	{
+		head: () => ({
+			meta: [
+				{
+					charSet: "utf-8",
+				},
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1",
+				},
+				{
+					title: "SnackFrancisco",
+				},
+			],
+			links: [
+				{
+					rel: "stylesheet",
+					href: appCss,
+				},
+			],
+			scripts: [
+				{
+					src: `https://maps.googleapis.com/maps/api/js?key=${env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`,
+					async: true,
+					defer: true,
+				},
+			],
+		}),
+		component: RootComponent,
+	},
+)
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	)
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	)
 }
